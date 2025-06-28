b7c25b5d5a629c50e30035cf064ff3f3e6007e1d
NEERAJ765 <kunaneeraj2004@gmail.com>
2025-06-28 22:46:51 +0530

    Fix path in prediction.py

--- prediction.py
+++ prediction.py
@@ -1,7 +1,7 @@
 import numpy as np;
 import pickle
 
-loaded_model = pickle.load(open('C:/Users/kunan/OneDrive/Desktop/cse/VS CODE/ml/projects/wine-quality/trained_model.sav','rb'))
+loaded_model = pickle.load(open('trained_model.sav','rb'))
 
 input_data = (7.4,0.7,0,1.9,0.076,11,34,0.9978,3.51,0.56,9.4)
 input_data_as_numpy_array = np.asarray(input_data)
